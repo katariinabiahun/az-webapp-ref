793276dbfd7e742386157a5acbf93e4472dfb30a
Katariina Biahun <9463011@gmail.com>
2023-03-27 15:03:00 +0300

    upd

--- webapp/webapp.tf
+++ webapp/webapp.tf
@@ -16,7 +16,7 @@ locals {
 }
 
 resource "azurerm_resource_group" "example" {
-  count = local.resource_group_name ? 0 : 1
+  count = var.resource_group_name ? 0 : 1
 
   name     = var.resource_group_name
   location = var.location
